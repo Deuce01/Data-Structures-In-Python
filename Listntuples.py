86d5c747473c44b1c4a37f29fbbbe48d076ffb02
VICTOR CHEGE <98538799+Deuce01@users.noreply.github.com>
2024-03-26 21:19:40 +0300

    Create Listntuples.py

---
+++ Listntuples.py
@@ -0,0 +1,10 @@
+# Lists
+fruits = ['apple', 'banana', 'cherry']
+print(fruits)
+fruits.append('orange')
+print(fruits)
+
+# Tuples
+colors = ('red', 'green', 'blue')
+print(colors)
+print(colors[1])
