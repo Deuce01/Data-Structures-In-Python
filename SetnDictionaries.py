75b413a79ba3a7e9adebbb4a6c54b0482e592dc9
VICTOR CHEGE <98538799+Deuce01@users.noreply.github.com>
2024-03-26 21:20:52 +0300

    Create SetnDictionaries.py

---
+++ SetnDictionaries.py
@@ -0,0 +1,10 @@
+# Sets
+unique_numbers = {1, 2, 3, 4, 5}
+print(unique_numbers)
+unique_numbers.add(6)
+print(unique_numbers)
+
+# Dictionaries
+student = {'name': 'Alice', 'age': 20, 'major': 'Computer Science'}
+print(student)
+print(student['age'])
