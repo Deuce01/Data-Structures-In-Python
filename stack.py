aaef75017ac4966cde4bb6399eb4116124d3f01d
VICTOR CHEGE <98538799+Deuce01@users.noreply.github.com>
2024-03-28 20:32:37 +0300

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,25 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+# Example Usage
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack.pop())
+print(stack.peek())
