d7a800ab5d401a456e79b59a4046f1b52c1fbf37
VICTOR CHEGE <98538799+Deuce01@users.noreply.github.com>
2024-03-28 20:35:23 +0300

    Create singlylinkedlist.py

---
+++ singlylinkedlist.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+# Example Usage
+llist = LinkedList()
+llist.append(1)
+llist.append(2)
+llist.append(3)
